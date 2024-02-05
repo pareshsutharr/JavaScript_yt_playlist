2caba265efb3bb69c2fe42bfb7a12aa3dc54a276
PARESH SUTHAR <129194145+pareshsutharr@users.noreply.github.com>
2024-02-05 08:16:00 +0000

    initial

---
+++ javascript_tutorial/9_string.js
@@ -0,0 +1,56 @@
+// let str = `'lw"s'`; //double quotes
+// let str1 = 'lws1'; //single quotes
+
+// console.log(str,str1)
+
+// let name = "lws";
+
+// console.log(`welcome to ${name} yt channel ${name}`); 
+
+
+// // string manipulation
+
+// let str = "welcome"; //0 to n-1 indexing // 1 to n length
+
+// console.log(str[1]);
+
+// // let n = str.length;   //length
+// console.log(n)
+
+// let str1 = "welcome to ";
+// let str2 = "lws";
+
+// let str3 = str1.concat(str2);  //concate
+// console.log(str3);
+
+// let str = "javascript";
+
+// let str2 = str.slice(4,8);   //slice method
+// console.log(str2)
+
+
+// let str = " JAVAscript";
+
+// console.log(str)
+// // console.log(str.toLowerCase());    lower case and upper case
+// console.log(str.toUpperCase());
+
+// let str = "       helloworld     "; // trim method
+// console.log(str.trim());
+
+
+// let str = "welcome paresh lws";    //replace method
+
+// console.log(str);
+// console.log(str.replace("paresh","to"));
+
+// let str = "lws";                  //repeat method
+
+// console.log(str.repeat(10));
+
+
+let str = "lws";
+console.log(str.charAt(2))
+
+
+
