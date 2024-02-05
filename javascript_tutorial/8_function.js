9bdccf2a1a3d0f3addf9679007a368b4b22b0d6c
PARESH SUTHAR <129194145+pareshsutharr@users.noreply.github.com>
2024-02-05 03:12:30 +0000

    8_function

---
+++ javascript_tutorial/8_function.js
@@ -0,0 +1,64 @@
+// let a = 10;
+// let b = 20;
+
+// function add(x,y){
+//     return x-y;
+// }
+
+// console.log("addition :" ,add(a,b));
+// console.log("addition :" ,add(a,b));
+// console.log("addition :" ,add(a,b));
+// console.log("addition :" ,add(a,b));
+// console.log("addition :" ,add(a,b));
+// console.log("addition :" ,a+b);
+// console.log("addition :" ,a+b);
+// console.log("addition :" ,a+b);
+// console.log("addition :" ,a-b);
+// console.log("addition :" ,a-b);
+// console.log("addition :" ,a+b);
+
+
+// function message(){
+//     console.log("hello javascript");
+// }
+// message();
+
+// let a=10,b=20;
+
+// function add(x,y){
+//     return x+y;
+// }
+// function sub(x,y){
+//     return x-y;
+// }
+// function div(x,y){
+//     return x/y;
+// }
+// function mul(x,y){
+//     return x*y;
+// }
+
+// console.log(add(a,b));
+// console.log(sub(a,b));
+// console.log(mul(a,b));
+// console.log(div(a,b));
+// let a=10,b=20;
+
+// // arrow function
+
+// const add=(t,b)=>{
+//     return t+b
+// }
+
+// let sum = add(a,b);
+// console.log(sum);
+
+
+// let names = "ankit";
+
+// function msgbox(userName){
+//     let str = `Dear ${userName} you have successfully joined our team`;
+//     console.log(str);
+// }
+
+// msgbox(names);
