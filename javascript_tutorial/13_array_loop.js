6804e5565f81cfa3dc55b3a80ac9427123456ad0
PARESH SUTHAR <pareshsutharr@gmail.com>
2024-02-09 20:26:36 +0530

    9 feb

---
+++ javascript_tutorial/13_array_loop.js
@@ -0,0 +1,20 @@
+// let company = ["google","microsoft","apple"];
+
+// // let payment = [100,200,300];
+
+// company.forEach((el)=>
+// console.log(el + 1)
+// );
+
+// let company = ["google","microsoft","apple"];
+
+// for(element of company){
+//     console.log(element);
+// }
+
+
+// let company = ["google","microsoft","apple"];
+
+// for(let i=0;i<company.length;i++){
+//     console.log(company[i]);
+// }
