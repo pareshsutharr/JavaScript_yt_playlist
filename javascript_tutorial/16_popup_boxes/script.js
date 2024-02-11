a30ba03dace908d119b3c6472616d85983385d60
PARESH SUTHAR <pareshsutharr@gmail.com>
2024-02-11 10:01:36 +0530

    11 feb

---
+++ javascript_tutorial/16_popup_boxes/script.js
@@ -0,0 +1,17 @@
+alert("welcome to lws")
+
+// let a = prompt("Enter a number : ");
+// alert(typeof a);
+
+// let age = prompt("enter your age : ");
+
+// alert(age>18?"you can drive" : "you cannot drive");
+
+//  let a = confirm("are you sure you want to leave");
+
+
+// if(confirm("want to leave this site")){
+//     window.location = "https://www.google.com";
+// }else{
+//     null
+// }
