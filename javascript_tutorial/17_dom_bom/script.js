8901970b34f745333a27b7afac5628abd8ef9142
PARESH SUTHAR <pareshsutharr@gmail.com>
2024-02-13 09:25:15 +0530

    13feb

---
+++ javascript_tutorial/17_dom_bom/script.js
@@ -0,0 +1,14 @@
+// window.alert("hello");
+
+window.console.log("code now")
+
+console.log(document)
+
+if(confirm("bg change to yellow")){
+document.body.style.background = "yellow"
+}else{
+    null
+}
+
+
+location.href = "https://www.google.com";
